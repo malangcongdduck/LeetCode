08588d6321c7b37ecd4761eabf8def7080ef7339
malangcongdduck <76987629+malangcongdduck@users.noreply.github.com>
2021-11-06 09:28:58 +0900

    Time: 32 ms (73.53%), Space: 14.4 MB (19.84%) - LeetHub

---
+++ house-robber/house-robber.py
@@ -0,0 +1,23 @@
+nums=[2,7,9,3,1]
+ans=[2,7,11,11,12]
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        ans=[0 for _ in range(len(nums))]
+        
+        if len(nums)==0:
+            return 0
+        elif len(nums)==1:
+            ans[0]=nums[0]
+        elif len(nums)==2:
+            ans[0]=nums[0]
+            ans[1]=max(nums[0],nums[1])
+        elif len(nums)>=3:
+            ans[0]=nums[0]
+            ans[1]=max(nums[0],nums[1])
+                
+            for i in range(2,len(nums)):
+                ans[i]=max(ans[i-1],ans[i-2]+nums[i])
+        
+        return ans[len(nums)-1]

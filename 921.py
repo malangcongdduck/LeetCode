ecdff4c0bf57683f1ca40c6408c77fea6fcd3398
malangcongdduck <76987629+malangcongdduck@users.noreply.github.com>
2021-11-06 09:10:25 +0900

    Create 921.py
    Minimum Add to Make Parentheses Valid

---
+++ 921.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        stack=[]
+        
+        for i in s:
+            
+            if i == '(':
+                stack.append(i)
+            elif i == ')':
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    count+=1
+                    
+        count = count + len(stack)
+        return count
